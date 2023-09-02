eb8c06332e30e3ccb66bd6b06addb573afa90ef2
Nusret Onur AYDUMAN <testerof@hotmail.com>
2023-09-02 18:47:48 +0300

    sumup0209

--- HackerRank/HackerRank.cs
+++ HackerRank/HackerRank.cs
@@ -53,7 +53,8 @@ class Solution
         //Interfaces.Starter(args);
         //FunnyString.Starter(args);
         //Gemstones.Starter(args);
-        Sorting.Starter(args);
+        //Sorting.Starter(args);
+        CleanNodes.Starter(args);
         Console.ReadLine();
     }
 }
@@ -1425,5 +1426,66 @@ class Sorting
         Console.WriteLine($"Array is sorted in {swaps} swaps.");
         Console.WriteLine($"First Element: {a[0]}");
         Console.WriteLine($"Last Element: {a[a.Count - 1]}");
+    }
+}
+
+class CleanNodes
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+
+    }
+    public static Node removeDuplicates(Node head)
+    {
+        //Write your code here
+    }
+
+    public static Node insert(Node head, int data)
+    {
+        Node p = new Node(data);
+
+
+        if (head == null)
+            head = p;
+        else if (head.next == null)
+            head.next = p;
+        else
+        {
+            Node start = head;
+            while (start.next != null)
+                start = start.next;
+            start.next = p;
+
+        }
+        return head;
+    }
+    public static void display(Node head)
+    {
+        Node start = head;
+        while (start != null)
+        {
+            Console.Write(start.data + " ");
+            start = start.next;
+        }
+    }
+    static void Starter(String[] args)
+    {
+
+        Node head = null;
+        int T = Int32.Parse(Console.ReadLine());
+        while (T-- > 0)
+        {
+            int data = Int32.Parse(Console.ReadLine());
+            head = insert(head, data);
+        }
+        head = removeDuplicates(head);
+        display(head);
     }
 }
